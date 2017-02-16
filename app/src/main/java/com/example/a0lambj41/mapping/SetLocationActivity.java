ff15140b90057619db4c692793ea10d71c60587f
jakelambert1 <Fm04lhz>
2017-02-16 12:21:23 +0000

    SetLocation2

--- app/src/main/java/com/example/a0lambj41/mapping/SetLocationActivity.java
+++ app/src/main/java/com/example/a0lambj41/mapping/SetLocationActivity.java
@@ -1,13 +1,43 @@
 package com.example.a0lambj41.mapping;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class SetLocationActivity extends Activity {
+public class SetLocationActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_location);
+
+        Button submitButton = (Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener (this);
+    }
+
+    @Override
+    public void onClick (View view){
+        //retreive our lat and long values from the edit teext boxes
+        EditText latitudeEditText = (EditText) findViewById(R.id.LatitudeEditText);
+        double latitude = Double.parseDouble(latitudeEditText.getText().toString());
+
+        EditText longitudeEditText = (EditText) findViewById(R.id.LongitudeEditText);
+        double longitude = Double.parseDouble(latitudeEditText.getText().toString());
+
+        //assemble our lat long bundle
+        Bundle latlongBundle = new Bundle();
+        latlongBundle.putDouble("com.0lambj41.latitude", latitude);
+        latlongBundle.putDouble("com.0lambj41.longitude", longitude);
+
+        //send the bundle to the parent activity
+        Intent intent = new Intent;
+        intent.putExtras(latlongBundle);
+        setResult(RESULT_OK, intent);
+        finish();
+
     }
 }
