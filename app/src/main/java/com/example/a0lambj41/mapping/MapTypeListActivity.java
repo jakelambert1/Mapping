d7c3f77e26ddb99f1ba047f8dc7022f8732d1174
0lambj41 <jakelambert1@hotmail.co.uk>
2017-02-23 12:44:35 +0000

    Map Type List Activity

--- app/src/main/java/com/example/a0lambj41/mapping/MapTypeListActivity.java
+++ app/src/main/java/com/example/a0lambj41/mapping/MapTypeListActivity.java
@@ -16,6 +16,7 @@ public class MapTypeListActivity extends ListActivity
     private String[] descriptions;
     String[] names;
 
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
